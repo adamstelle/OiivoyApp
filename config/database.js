6cdb518ecdf705fc9f93f94b9b543e5ff3aea1f0
Adam Stelle <adamstelle@gmail.com>
2015-02-18 15:42:22 -0800

    Added all files

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://test:poop@proximus.modulusmongo.net:27017/xanEh3an' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
